7102a262a5ad275ea9b5e71d8d714f0bd3f1661e
Moshe <moisherosenzweig@gmail.com>
2021-12-20 20:56:06 -0500

    Mission Complete

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b) {
+    return a + b
+
+}
+function subtract() {
+    
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(){
+
+}
+
+function multiply(a, b){
+    return a * b
+
+}
+
+function divide(){
+    
+}
+
+function divide(a , b){
+    return a / b
+}
+
+
+function increment(n) {
+    return ++n
+}
+
+function decrement(n) {
+    return --n
+}
+
+function makeInt (moose) {
+    return parseInt (moose, 10)
+}
+
+function preserveDecimal (Swag) {
+     return parseFloat (Swag, 10);
+}
+
+
+
+
